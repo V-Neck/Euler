1a999b4b54a2cba51397ce99c20b331603f0cbd9
Vaughn Johnson <evilepicproductions@gmail.com>
2017-11-23 16:11:10 -0800

    Very slow. Should look into optimizations.

---
+++ prob8.py
@@ -0,0 +1,24 @@
+import numpy as np
+from prob3 import is_prime
+
+def first_n_primes(n):
+    # By definition
+    primes = [2]
+
+    # Check if m is prime
+    m = 3
+
+    while len(primes) < n:
+        is_prime = True
+        for p in primes:
+            if m % p == 0:
+                is_prime = False
+                break
+        if is_prime:
+            # Doesn't make sense to use a generator, because you store the list anyway
+            primes.append(m)
+        m += 2
+    return primes
+
+
+first_n_primes(10001)[-1]
