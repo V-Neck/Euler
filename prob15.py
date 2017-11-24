2e2d24ac1a122559ebc23aa6839d346a9463a9dc
Vaughn Johnson <evilepicproductions@gmail.com>
2017-11-24 15:18:35 -0800

    Now I have a factorial module

--- prob15.py
+++ prob15.py
@@ -12,4 +12,5 @@ from operator import mul
 def fact(n):
     return reduce(mul, range(1, n+1))
 
-fact(40) / (fact(20) * fact(20))
+if __name__ == "__main__":
+    print fact(40) / (fact(20) * fact(20))
