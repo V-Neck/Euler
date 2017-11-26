0c34e40241240d470d57873a05852a34125dd27c
Vaughn Johnson <evilepicproductions@gmail.com>
2017-11-26 02:01:10 -0800

    Very easy problem

---
+++ prob21.py
@@ -0,0 +1,34 @@
+# Let d(n) be defined as the sum of proper divisors of n
+# (numbers less than n which divide evenly into n).
+# If d(a) = b and d(b) = a, where a ≠ b,
+# then a and b are an amicable pair and each of a and b are called amicable numbers.
+#
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110;
+# therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+#
+# Evaluate the sum of all the amicable numbers under 10000
+
+from math import sqrt
+from numpy import transpose
+
+def sum_prop_facts(n):
+    if n < 1:
+        raise ValueError
+    factors = [1]
+    for i in range(2, int(sqrt(n))):
+        if (n % i) == 0:
+            factors.append(i)
+            factors.append(n/i)
+    return sum(factors)
+
+N = 10000
+
+if __name__ == "__main__":
+    amicables = []
+·
+    for n in range(1, N):
+        a = sum_prop_facts(n)
+        if sum_prop_facts(a) == n and a != n:
+            amicables.append(n)
+
+    print sum(amicables)
